12a874ee5d07e3e7f5c3cc5b6f8c1be3a5f1f483
Joel <iamjoel007@gmail.com>
2023-12-22 18:31:51 +0800

    feat: change logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -69,7 +69,7 @@ const config = {
         title: 'Dify',
         logo: {
           alt: 'Dify Logo',
-          src: 'img/logo.svg',
+          src: 'img/logo.png',
         },
         items: [
           // i18n dropdown
